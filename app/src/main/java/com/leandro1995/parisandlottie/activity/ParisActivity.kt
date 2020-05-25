c30fddbb4d3deba455e711f84563e10000f194e9
Leandro Castillo Borja <leccbo1995@gmail.com>
2020-05-25 18:12:15 -0500

    Implementar paris con eventos basicos

--- app/src/main/java/com/leandro1995/parisandlottie/activity/ParisActivity.kt
+++ app/src/main/java/com/leandro1995/parisandlottie/activity/ParisActivity.kt
@@ -2,12 +2,26 @@ package com.leandro1995.parisandlottie.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.airbnb.paris.extensions.style
 import com.leandro1995.parisandlottie.R
+import kotlinx.android.synthetic.main.activity_paris.*
 
 class ParisActivity : AppCompatActivity() {
+
+    private var status = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_paris)
+
+        actionButton.setOnClickListener {
+            status = if (status) {
+                styleButton.style(R.style.StyleOne)
+                false
+            } else {
+                styleButton.style(R.style.StyleTwo)
+                true
+            }
+        }
     }
 }
